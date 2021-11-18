dd993178dab7d016b5e276ddfaacbe6a872358ee
Auk-A <auke.aardoom@gmail.com>
2021-11-19 00:32:47 +0100

    Tekst functionaliteit

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ display = pygame.display.set_mode((800, 600), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 
 
+class Text:
+    def __init__(self, font):
+        self.text = ""
+        self.font = font
+
+    def change_font(self, font):
+        self.font = font
+
+    def change_text(self, new_text):
+        self.text = new_text
+
+    def render(self, location):
+        text_surface = pygame.font.SysFont(self.font, 30).render(self.text, True, (255, 255, 255))
+        display.blit(text_surface, dest=location)
+
+
 class Player:
     def __init__(self, x, y, width, height):
         self.x = x
@@ -21,7 +37,8 @@ class Player:
         pygame.draw.rect(display_area, (255, 0, 0), (self.x, self.y, self.width, self.height))
 
 
-player = Player(400, 300, 32, 32)
+# player = Player(400, 300, 32, 32)
+mainText = Text("Calibri")
 
 
 while True:
@@ -31,8 +48,8 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    player.main(display)
-
+    mainText.render((100, 100))
+    # player.main(display)
 
     clock.tick(60)
     pygame.display.update()
